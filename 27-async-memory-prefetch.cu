149ec96c74601461073f44bca5f3d6a434876cb1
Longwei Su <longwei.su@gmail.com>
2025-07-27 21:36:17 -0400

    vector add

---
+++ 27-async-memory-prefetch.cu
@@ -0,0 +1,4 @@
+// Asynchronous Memory Prefetching
+
+// This technique reduces overhead from page faults and on-demand memory migrations in host-to-device and device-to-host transfers.
+// It allows asynchronous migration of unified memory (UM) to any CPU or GPU in the system before use, improving GPU kernel and CPU function performance.
